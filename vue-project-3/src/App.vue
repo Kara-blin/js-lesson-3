f2b2fdd839b5ab50880a9516587dbb37c16667ac
Kara-blin <86315259+Kara-blin@users.noreply.github.com>
2023-04-21 21:57:54 +0300

    Initial commit

---
+++ vue-project-3/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      count: null,
+      info: {}
+    }
+  },
+  created() {
+    this.getToDos();
+  },
+  methods: {
+    getToDos() {
+      axios.get("https://jsonplaceholder.typicode.com/todos/")
+          .then(response => {
+            this.info = response.data;
+          });
+    },
+    deleteToDos(id) {
+      this.info.splice(id, 1);
+    }
+  }
+}
+</script>
+
+<template>
+  <td>
+    <input type="number" v-on:input="count = $event.target.value">
+    <button @click="deleteToDos(this.count)">Delete</button>
+  </td>
+  <h1>{{info}}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: black;
+}
+</style>
